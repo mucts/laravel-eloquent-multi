f3d5cc04bbcaad8aece19fcfc1796b07130ae926
herry <yuandeng@aliyun.com>
2020-05-12 15:19:02 +0800

    延时服务

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -4,10 +4,11 @@
 namespace MuCTS\Laravel\EloquentMulti\Providers;
 
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider as Providers;
 use MuCTS\Laravel\EloquentMulti\Commands\ModelMakeCommand;
 
-class ServiceProvider extends Providers
+class ServiceProvider extends Providers implements DeferrableProvider
 {
     public function boot()
     {
